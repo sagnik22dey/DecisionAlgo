25601d62fdd54d610009858e8779f1bd11ecabc8
sagnik22dey <sagnik22dey@gmail.com>
2025-09-08 01:08:00 +0530

    contact-us-start

--- Routes/contactUs.py
+++ Routes/contactUs.py
@@ -2,10 +2,8 @@ from fastapi import APIRouter
 from fastapi.responses import HTMLResponse
 from Components.general_components.navbar import *
 from Components.general_components.footer import *
-from Components.aboutUs_components.heroSection import *
-from Components.aboutUs_components.comapnies import *
-from Components.aboutUs_components.innovation import *
-from Components.aboutUs_components.team import *
+from Components.contactUs_components.heroSection import *
+
 
 
 
@@ -14,6 +12,14 @@ router = APIRouter()
 
 @router.get("/contactus", response_class=HTMLResponse)
 async def contactUs():
+    navbar_html = await navbar_body()
+    footer_html = await footer_body()
+    navbar_css = await navbar_style()
+    footer_css = await footer_style()
+    heroSection_html = await heroSection_body()
+    heroSection_css = await heroSection_style()
+    
+    
     return f"""
     <html>
         <head>
@@ -28,10 +34,17 @@ async def contactUs():
         body {{
             overflow-x: hidden;
         }}
-        </style>
+        {navbar_css}
+        {footer_css}
+        {heroSection_css}
         
+        
+        </style>
         </head>
         <body>
+        {navbar_html}
+        {heroSection_html}
+        {footer_html}
             
             
             
